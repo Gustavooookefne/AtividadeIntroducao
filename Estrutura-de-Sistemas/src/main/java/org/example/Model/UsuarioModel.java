6f3cf4a8c4ae9a1cc1c5f0e33176d7051104b910
Gustavooookefne <gustavooookefne14@gmail.com>
2025-11-03 19:39:44 -0300

    feat: Criando a parte do UsuarioModel

--- Estrutura-de-Sistemas/src/main/java/org/example/Model/UsuarioModel.java
+++ Estrutura-de-Sistemas/src/main/java/org/example/Model/UsuarioModel.java
@@ -1,9 +1,44 @@
 package org.example.Model;
 
 public class UsuarioModel {
+    private int id;
     private String nome;
     private String Email;
 
+    public UsuarioModel(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        Email = email;
+    }
+
+    public UsuarioModel(String nome, String email) {
+        this.nome = nome;
+        Email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
 }
 
 
