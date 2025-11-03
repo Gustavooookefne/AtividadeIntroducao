b42f3b56d35a19783af95a4d5c931ef510221515
Gustavooookefne <gustavooookefne14@gmail.com>
2025-11-03 19:42:45 -0300

    feat: Criando a parte do LivrosModel

--- Estrutura-de-Sistemas/src/main/java/org/example/Model/LivroModel.java
+++ Estrutura-de-Sistemas/src/main/java/org/example/Model/LivroModel.java
@@ -1,4 +1,67 @@
 package org.example.Model;
 
 public class LivroModel {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String ano;
+    private boolean disponivel;
+
+    public LivroModel(int id, String titulo, String autor, String ano, boolean disponivel) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponivel = disponivel;
+    }
+
+    public LivroModel(String titulo, String autor, String ano, boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponivel = disponivel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+
 }
+
