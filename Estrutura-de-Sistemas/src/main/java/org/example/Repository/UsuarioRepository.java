5f1a5d568cfc534883cf42d5132f6d437f2509c1
Gustavooookefne <gustavooookefne14@gmail.com>
2025-11-03 21:06:50 -0300

    feat: Criando o UsuarioRepository

--- Estrutura-de-Sistemas/src/main/java/org/example/Repository/UsuarioRepository.java
+++ Estrutura-de-Sistemas/src/main/java/org/example/Repository/UsuarioRepository.java
@@ -1,5 +1,30 @@
 package org.example.Repository;
 
+import org.example.Infrestrutura.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UsuarioRepository {
+    public static boolean devolverLivro (int idLivro)throws SQLException{
+        String query = """
+                UPDATE livro
+                SET status = 0
+                WHERE id = ? AND status = 1
+                """;
+
+        try(Connection conn = Conexao.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setInt(1,idLivro);
+
+            int rowsUpadated = stmt.executeUpdate();
+
+            return rowsUpadated > 0;
+
+        }
+
+    }
 
 }
