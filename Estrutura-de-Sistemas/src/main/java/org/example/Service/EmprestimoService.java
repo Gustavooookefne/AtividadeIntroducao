409ceaf82985fa5eb1340074c8c81ebaa11955e6
Gustavooookefne <gustavooookefne14@gmail.com>
2025-11-03 21:16:37 -0300

    feat: Criando O EmprestimoSerive

--- Estrutura-de-Sistemas/src/main/java/org/example/Service/EmprestimoService.java
+++ Estrutura-de-Sistemas/src/main/java/org/example/Service/EmprestimoService.java
@@ -1,4 +1,21 @@
 package org.example.Service;
 
+import org.example.Model.Emprestimo;
+import org.example.Repository.EmprestimoRepository;
+
+import java.sql.SQLException;
+
 public class EmprestimoService {
+
+    private final EmprestimoRepository emprestimoRepository = new EmprestimoRepository();
+
+    public void CricarEmprestimo(Emprestimo emprestimo){
+        try{
+            EmprestimoRepository.inserirEmprestimo(emprestimo);
+        }catch (SQLException e){
+            System.out.println("Falha ao Criar Emprestimo!!");
+            e.printStackTrace();
+        }
+    }
+
 }
