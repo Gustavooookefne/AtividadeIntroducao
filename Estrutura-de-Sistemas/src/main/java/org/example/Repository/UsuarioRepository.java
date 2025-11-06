8ed9b8ad2c4255fe74841f06ee620f0f4ab4566c
Gustavooookefne <gustavooookefne14@gmail.com>
2025-11-06 19:32:13 -0300

    feat: Criando o Registrar Emprestimo no Repository

--- Estrutura-de-Sistemas/src/main/java/org/example/Repository/UsuarioRepository.java
+++ Estrutura-de-Sistemas/src/main/java/org/example/Repository/UsuarioRepository.java
@@ -27,4 +27,22 @@ public class UsuarioRepository {
 
     }
 
+    public static boolean registrarEmprestimo(int idEmprestimo)throws SQLException{
+        String query = """
+                UPDATE emprestimos
+                SET status = 0
+                WHERE id = ? AND status = 1
+                """;
+
+        try(Connection conn = Conexao.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setInt(1, idEmprestimo);
+
+            int rowsUpadated = stmt.executeUpdate();
+
+            return rowsUpadated > 0;
+        }
+    }
+
 }
